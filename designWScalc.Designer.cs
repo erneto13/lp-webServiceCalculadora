930886cda1f8edf4aab16fca344a65d10b9e622b
erneto13 <erneto.amaral13@gmail.com>
2023-10-20 02:13:13 -0700

    Actualización v0.1

---
+++ designWScalc.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace lp_webService
+{
+    partial class designWScalc
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(designWScalc));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.documentaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.irAlRepositorioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lblNumeroA = new System.Windows.Forms.Label();
+            this.lblNumeroB = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtNumeroA = new System.Windows.Forms.TextBox();
+            this.txtNumeroB = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.documentaciónToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(304, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // documentaciónToolStripMenuItem
+            // 
+            this.documentaciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.irAlRepositorioToolStripMenuItem});
+            this.documentaciónToolStripMenuItem.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.documentaciónToolStripMenuItem.Name = "documentaciónToolStripMenuItem";
+            this.documentaciónToolStripMenuItem.Size = new System.Drawing.Size(118, 26);
+            this.documentaciónToolStripMenuItem.Text = "Documentación";
+            // 
+            // irAlRepositorioToolStripMenuItem
+            // 
+            this.irAlRepositorioToolStripMenuItem.Name = "irAlRepositorioToolStripMenuItem";
+            this.irAlRepositorioToolStripMenuItem.Size = new System.Drawing.Size(167, 26);
+            this.irAlRepositorioToolStripMenuItem.Text = "Ir al repositorio";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lblNumeroA
+            // 
+            this.lblNumeroA.AutoSize = true;
+            this.lblNumeroA.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroA.Location = new System.Drawing.Point(22, 44);
+            this.lblNumeroA.Name = "lblNumeroA";
+            this.lblNumeroA.Size = new System.Drawing.Size(65, 19);
+            this.lblNumeroA.TabIndex = 2;
+            this.lblNumeroA.Text = "Numero A:";
+            // 
+            // lblNumeroB
+            // 
+            this.lblNumeroB.AutoSize = true;
+            this.lblNumeroB.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroB.Location = new System.Drawing.Point(22, 75);
+            this.lblNumeroB.Name = "lblNumeroB";
+            this.lblNumeroB.Size = new System.Drawing.Size(65, 19);
+            this.lblNumeroB.TabIndex = 3;
+            this.lblNumeroB.Text = "Numero B:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(22, 139);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(69, 19);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "Resultado: ";
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(12, 183);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(65, 31);
+            this.btnSumar.TabIndex = 5;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Location = new System.Drawing.Point(83, 183);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(65, 31);
+            this.btnRestar.TabIndex = 6;
+            this.btnRestar.Text = "Restar";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(154, 183);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(65, 31);
+            this.btnMultiplicar.TabIndex = 7;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(225, 183);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(65, 31);
+            this.btnDividir.TabIndex = 8;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(182, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtNumeroA
+            // 
+            this.txtNumeroA.Location = new System.Drawing.Point(93, 42);
+            this.txtNumeroA.Name = "txtNumeroA";
+            this.txtNumeroA.Size = new System.Drawing.Size(71, 20);
+            this.txtNumeroA.TabIndex = 10;
+            // 
+            // txtNumeroB
+            // 
+            this.txtNumeroB.Location = new System.Drawing.Point(93, 75);
+            this.txtNumeroB.Name = "txtNumeroB";
+            this.txtNumeroB.Size = new System.Drawing.Size(71, 20);
+            this.txtNumeroB.TabIndex = 11;
+            // 
+            // designWScalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 226);
+            this.Controls.Add(this.txtNumeroB);
+            this.Controls.Add(this.txtNumeroA);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblNumeroB);
+            this.Controls.Add(this.lblNumeroA);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "designWScalc";
+            this.Text = "WebService | Calculadora";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem documentaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem irAlRepositorioToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label lblNumeroA;
+        private System.Windows.Forms.Label lblNumeroB;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtNumeroA;
+        private System.Windows.Forms.TextBox txtNumeroB;
+    }
+}
+
---
+++ designWScalc.cs
@@ -0,0 +1,155 @@
+﻿using lp_webService.ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lp_webService
+{
+    public partial class designWScalc : Form
+    {
+        public designWScalc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            // Instancia del proxy para el web service
+            ServiceReference.CalculatorSoapClient calculadora = new ServiceReference.CalculatorSoapClient();
+
+            // Verificar que no haya cadena vacía en el txtNúmeroA & txtNúmeroB
+            if (string.IsNullOrEmpty(txtNumeroA.Text) ) {
+                MessageBox.Show("Rellena el campo del Número A.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); // Cuadro de error de cadena vacia
+            }
+            else if (string.IsNullOrEmpty(txtNumeroB.Text) ) // Verificació para cadena vacía de txtNúmeroB
+            {
+                MessageBox.Show("Rellena el campo del Número B.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    // Se convierten a entero las entradas de los txtBox
+                    int valorA = Int32.Parse(txtNumeroA.Text);
+                    int valorB = Int32.Parse(txtNumeroB.Text);
+
+                    // Llamar al método sumar del web service
+                    int result = calculadora.Add(valorA, valorB);
+                    lblResultado.Text = "Resultado: " + result.ToString();
+                }
+                catch (FormatException)
+                {
+                    // Error por un número no válido
+                    MessageBox.Show("La entrada no es un número entero válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            // Instancia del proxy para el web service
+            ServiceReference.CalculatorSoapClient calculadora = new ServiceReference.CalculatorSoapClient();
+
+            if (string.IsNullOrEmpty(txtNumeroA.Text))
+            {
+                MessageBox.Show("Rellena el campo del Número A.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (string.IsNullOrEmpty(txtNumeroB.Text))
+            {
+                MessageBox.Show("Rellena el campo del Número B.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    // Se convierten a entero las entradas de los txtBox
+                    int valorA = Int32.Parse(txtNumeroA.Text);
+                    int valorB = Int32.Parse(txtNumeroB.Text);
+
+                    // Llamar al método sumar del web service
+                    int result = calculadora.Subtract(valorA, valorB);
+                    lblResultado.Text = "Resultado: " + result.ToString();
+                }
+                catch (FormatException)
+                {
+                    // Error por un número no válido
+                    MessageBox.Show("La entrada no es un número entero válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            // Instancia del proxy para el web service
+            ServiceReference.CalculatorSoapClient calculadora = new ServiceReference.CalculatorSoapClient();
+
+            if (string.IsNullOrEmpty(txtNumeroA.Text))
+            {
+                MessageBox.Show("Rellena el campo del Número A.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (string.IsNullOrEmpty(txtNumeroB.Text))
+            {
+                MessageBox.Show("Rellena el campo del Número B.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    // Se convierten a entero las entradas de los txtBox
+                    int valorA = Int32.Parse(txtNumeroA.Text);
+                    int valorB = Int32.Parse(txtNumeroB.Text);
+
+                    // Llamar al método sumar del web service
+                    int result = calculadora.Multiply(valorA, valorB);
+                    lblResultado.Text = "Resultado: " + result.ToString();
+                }
+                catch (FormatException)
+                {
+                    // Error por un número no válido
+                    MessageBox.Show("La entrada no es un número entero válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            // Instancia del proxy para el web service
+            ServiceReference.CalculatorSoapClient calculadora = new ServiceReference.CalculatorSoapClient();
+
+            if (string.IsNullOrEmpty(txtNumeroA.Text))
+            {
+                MessageBox.Show("Rellena el campo del Número A.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (string.IsNullOrEmpty(txtNumeroB.Text))
+            {
+                MessageBox.Show("Rellena el campo del Número B.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    // Se convierten a entero las entradas de los txtBox
+                    int valorA = Int32.Parse(txtNumeroA.Text);
+                    int valorB = Int32.Parse(txtNumeroB.Text);
+
+                    // Llamar al método sumar del web service
+                    int result = calculadora.Divide(valorA, valorB);
+                    lblResultado.Text = "Resultado: " + result.ToString();
+                }
+                catch (FormatException)
+                {
+                    // Error por un número no válido
+                    MessageBox.Show("La entrada no es un número entero válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+    }
+}
